7125554a828d28d5c69d2abd9160eb34af65079f
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-14 11:03:30 -0400

    ngn

--- Duel/src/duel/Dueler.java
+++ Duel/src/duel/Dueler.java
@@ -1,14 +1,28 @@
 package duel;
 
-public interface Dueler {
+public interface Dueler
 
+	this.name = name;
 	
+
 	//print a taunt
 	//later: the taunt must be different each time the method is called
-	void taunt();
+	void taunt();{
+		
+		System.out.println(taunts);
+	}
 	
 	//returns the name of the Dueler
-	String getName();
+	public String getName();{
+		
+		System.out.println(name);
+	}
+	//set the value of a field. Write this in such a way that it can only be called once (by the Duel, at the start)
+	void setStartingHP(int hp);
+	//returns the value of HP, can be called as often as anyone wants
+	int getHP();
 	
-	
+	//This method is called by the Duel and can be used by the dueler
+	//return true if d has hp of given level
+	boolean determineIfOpponentIsFair(Dueler d, int hp);
 }
