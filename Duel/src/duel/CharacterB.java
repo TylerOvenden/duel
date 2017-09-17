54ee83668cba16ee2c6839e05d13a3a70f964a0f
Tyler2017 <Tyler2017@LAPTOP-MELKJ2M8>
2017-09-17 12:39:56 -0400

    ssxx

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -4,9 +4,54 @@ public class CharacterB implements Dueler {
 	private int hp;
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+	@Override
+	public void taunt() {
+		// TODO Auto-generated method stub
+		System.out.println("You are bad at this");	
+	}
+
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return("doge");
+	}
+
+
+	@Override
+	public void setStartingHP(int hp) {
+		
+		
+	}
+
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public int getAction(Object caller) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public void hit(Object caller) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
