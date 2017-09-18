ca0ce4b888b834624eecd34e6c8619cb632f5abc
Remington <Remington@Renovo.T-mobile.com>
2017-09-18 00:49:34 -0400

    Updated CharacterA

--- Duel/src/duel/CharacterA.java
+++ Duel/src/duel/CharacterA.java
@@ -1,5 +1,70 @@
 package duel;
 
-public class CharacterA implements Duel {
+public class CharacterA implements Dueler {
+	private int hp;
+	
+	
+
+
+	@Override
+	public void taunt() {
+		if (Math.random() < .5) {
+		System.out.println("You can't possibly defeat me!");	
+		} 
+		else {
+		System.out.println("Haha! Try again?");
+		}
+		
+	}
+
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return("Quick R");
+	}
+
+
+	@Override
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return this.hp;
+	}
+
+
+	@Override
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		if (d.getHP() == 100) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	@Override
+	public int getAction(Object caller) {
+		// TODO Auto-generated method stub
+		caller = new TheDuel();
+		return caller;
+	}
+
+
+	@Override
+	public void hit(Object caller) {
+		// TODO Auto-generated method stub
+		if (caller.getClass() == TheDuel.class) {
+			this.hp -= 10;
+		}
+	}
+
+
 
 }
